cd9a7be9367e3aa5997f6c5c5b905f5d773280a6
tanyudii <y.hertanto17@gmail.com>
2021-06-07 00:35:02 +0700

    trial

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { useContainer } from 'class-validator';
 import { ConfigService } from '@nestjs/config';
+import * as helmet from 'helmet';
+import * as rateLimit from 'express-rate-limit';
 
 (async () => {
   const configService = new ConfigService();
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(), {
+    cors: true,
+  });
 
   //is used for transform pipes message
   app.useGlobalPipes(
@@ -19,6 +24,9 @@ import { ConfigService } from '@nestjs/config';
 
   //is used for allow custom pipes attribute
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
+
+  app.use(helmet());
+  app.use(rateLimit({ windowMs: 60 * 1000, max: 1000 }));
 
   await app.listen(configService.get<number>('APP_PORT') || 3010);
 })();
