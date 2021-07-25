2b0e7c458812808ef1a8cee72c9f3c2338207c0f
Yudi Hertanto <39409106+tanyudii@users.noreply.github.com>
2021-07-25 15:14:32 +0700

    update key

--- src/application/controllers/loader.controller.ts
+++ src/application/controllers/loader.controller.ts
@@ -2,8 +2,8 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class LoaderController {
-  @Get('loaderio-5f6a5bed467e7d14aa3ebc295f7589eb')
+  @Get('loaderio-9d96ee590937f923fa6f92cf93e4a96a')
   loaderTest() {
-    return 'loaderio-5f6a5bed467e7d14aa3ebc295f7589eb';
+    return 'loaderio-9d96ee590937f923fa6f92cf93e4a96a';
   }
 }
