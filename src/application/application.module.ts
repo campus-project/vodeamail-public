f1201950b8057cd1e4efbff893169683df151035
Yudi Hertanto <39409106+tanyudii@users.noreply.github.com>
2021-07-25 15:29:16 +0700

    update

--- src/application/application.module.ts
+++ src/application/application.module.ts
@@ -1,12 +1,12 @@
-import { Module } from '@nestjs/common';
-import { InfrastructureModule } from '../infrastructure/infrastructure.module';
-import { DomainModule } from '../domain/domain.module';
-import { EmailCampaignAnalyticController } from './controllers/email-campaign-analytic.controller';
-import { LoaderController } from './controllers/loader.controller';
+import { Module } from "@nestjs/common";
+import { InfrastructureModule } from "../infrastructure/infrastructure.module";
+import { DomainModule } from "../domain/domain.module";
+import { EmailCampaignAnalyticController } from "./controllers/email-campaign-analytic.controller";
+import { LoaderController } from "./controllers/loader.controller";
 
 @Module({
   imports: [InfrastructureModule, DomainModule],
   exports: [InfrastructureModule, DomainModule],
-  controllers: [EmailCampaignAnalyticController, LoaderController],
+  controllers: [LoaderController, EmailCampaignAnalyticController],
 })
 export class ApplicationModule {}
